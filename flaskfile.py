17e7923a7bf314caee1aad6e09f801cbf3a4b366
DiegoLB101296 <112820760+DiegoLB101296@users.noreply.github.com>
2022-11-15 09:26:39 -0600

    Add files via upload

---
+++ flaskfile.py
@@ -0,0 +1,21 @@
+import numpy as np
+import pickle
+from flask import Flask, render_template, request, jsonify
+
+app = Flask(__name__)
+model = pickle.load(open('model.pkl', 'rb'))
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/predict',methods=['POST'])
+def predict():
+    int_features = [float(x) for x in request.form.values()]
+    final_features = np.array(int_features).reshape(1,-1)
+    prediction = model.predict(final_features)
+    print(prediction)
+    return render_template('index.html', prediction_text='Predicted Category is {}'.format(prediction))
+
+if __name__ == "__main__":
+    app.run()
